ffa5a98af0286fa5132ea47cc2d3aecfab6a3d36
bryanpozo <bryanmanuelmedinapozo2015@gmail.com>
2020-10-04 21:44:42 -0400

    Add files via upload

---
+++ sumatoria/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sumatoria
+{
+    class Program
+    {
+        static int sumatoria(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            else
+            {
+                return n + sumatoria(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Ingresar la cantidad a sumar:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("La sumatoria es:" + sumatoria(n));
+            Console.ReadLine();
+        }
+    }
+}
