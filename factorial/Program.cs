31cf6b72f7dc0ae2ae5d281d30734e63c96d7d47
bryanpozo <bryanmanuelmedinapozo2015@gmail.com>
2020-10-04 21:47:03 -0400

    Add files via upload

---
+++ factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        public static long factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * factorial(n - 1);
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Ingrese el numero: ");
+            n = Convert.ToInt32(System.Console.ReadLine());
+
+            Console.WriteLine("Su factorial es: {0}",+factorial(n));
+            Console.ReadKey();
+        }
+    }
+}
